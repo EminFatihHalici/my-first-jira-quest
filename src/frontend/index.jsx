d78ca759ff6ad3563b927b4344aa5294092a26f5
Emin Fatih Halici <halicieminfatih@gmail.com>
2026-03-09 12:53:38 +0100

    feat: add user info retrieval based on account ID and update greeting with user's display name

--- src/frontend/index.jsx
+++ src/frontend/index.jsx
@@ -1,15 +1,19 @@
 import React, { useEffect, useState } from 'react';
 import ForgeReconciler, { Heading, Lozenge, Text } from '@forge/react';
-import { invoke, view } from '@forge/bridge';
+import { invoke, view, requestJira } from '@forge/bridge'
 
 const App = () => {
+  const [accountID, setAccountID] = useState(null);
+  const [name, setName] = useState(null);
   const [data, setData] = useState(null);
   const [theme, setTheme] = useState(null);
   useEffect(() => {
      const getTheme = async() => {
-     const context = await view.getContext();
-     console.log(context);
-     setTheme(context.theme.colorMode);
+      const context = await view.getContext();
+      console.log(context);
+      setTheme(context.theme.colorMode);
+      setAccountID(context.accountId);
+
    }
  getTheme();
 }, []);
@@ -18,9 +22,34 @@ useEffect(() => {
     invoke('getText', { example: 'my-invoke-variable' }).then(setData);
   }, []);
 
+  useEffect(() => {
+ const getUserInfo = async() => {
+   if(accountID) { 
+     const response = await requestJira(`/rest/api/2/user/search?accountId=${accountID}`, {
+       method: 'GET',
+       headers: {
+         'Accept': 'application/json',
+       }
+     });
+     if(response.status === 200) {
+       let data = await response.json();
+       console.log(data);
+       setName(data[0].displayName);
+     } else 
+     { 
+       console.log(response)
+     }
+   } else {
+     console.log("accountID is null")
+   }
+ }
+ getUserInfo();
+}, [accountID]);
+
+
   return (
     <>
-      <Heading size="xlarge">Hello world!</Heading>
+      <Heading size="xlarge">Hello {name ? name: 'World'}!</Heading>
       <Text>{data ? data : 'Loading...'}</Text>
       <Text>Current theme: <Lozenge>{theme ? theme : 'Loading...'}</Lozenge></Text>
     </>
